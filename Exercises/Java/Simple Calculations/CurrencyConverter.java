262cd8a95862f61fc777a8e88e191c604ba15282
Alex Stoyanov <loveca001@gmail.com>
2017-05-10 23:50:09 +0300

    Fixed code errors.

--- Exercises/Java/Simple Calculations/CurrencyConverter.java
+++ Exercises/Java/Simple Calculations/CurrencyConverter.java
@@ -16,48 +16,57 @@ public class CurrencyConverter {
         double poundRate = 2.53405;
 
         if (inputValue.equalsIgnoreCase("BGN")) {
-        } else if (outputValue.equalsIgnoreCase("USD")) {
-            double result = money / dollarRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("EUR")) {
-            double result = money / euroRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("GBP")) {
-            double result = money / poundRate;
-            System.out.println(round.format(result));
-        }
-        if (inputValue.equalsIgnoreCase("USD")) {
-        } else if (outputValue.equalsIgnoreCase("BGN")) {
-            double result = money * dollarRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("EUR")) {
-            double result = (money * dollarRate) / euroRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("GBP")) {
-            double result = (money * dollarRate) / poundRate;
-            System.out.println(round.format(result));
+            if (outputValue.equalsIgnoreCase("BGN")) {
+                System.out.println(money);
+            } else if (outputValue.equalsIgnoreCase("USD")) {
+                double result = money / dollarRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("EUR")) {
+                double result = money / euroRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("GBP")) {
+                double result = money / poundRate;
+                System.out.println(round.format(result));
+            }
+        } else if (inputValue.equalsIgnoreCase("USD")) {
+            if (outputValue.equalsIgnoreCase("USD")) {
+                System.out.println(money);
+            } else if (outputValue.equalsIgnoreCase("BGN")) {
+                double result = money * dollarRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("EUR")) {
+                double result = (money * dollarRate) / euroRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("GBP")) {
+                double result = (money * dollarRate) / poundRate;
+                System.out.println(round.format(result));
+            }
         } else if (inputValue.equalsIgnoreCase("EUR")) {
-        } else if (outputValue.equalsIgnoreCase("BGN")) {
-            double result = money * euroRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("USD")) {
-            double result = (money * euroRate) / dollarRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("GBP")) {
-            double result = (money * euroRate) / poundRate;
-            System.out.println(round.format(result));
-        }
-        if (inputValue.equalsIgnoreCase("GBP")) {
-        } else if (outputValue.equalsIgnoreCase("BGN")) {
-            double result = money * poundRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("USD")) {
-            double result = (money * poundRate) / dollarRate;
-            System.out.println(round.format(result));
-        } else if (outputValue.equalsIgnoreCase("EUR")) {
-            double result = (money * poundRate) / euroRate;
-            System.out.println(round.format(result));
+            if (outputValue.equalsIgnoreCase("EUR")) {
+                System.out.println(money);
+            } else if (outputValue.equalsIgnoreCase("BGN")) {
+                double result = money * euroRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("USD")) {
+                double result = (money * euroRate) / dollarRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("GBP")) {
+                double result = (money * euroRate) / poundRate;
+                System.out.println(round.format(result));
+            }
+        } else if (inputValue.equalsIgnoreCase("GBP")) {
+            if (outputValue.equalsIgnoreCase("GBP")) {
+                System.out.println(money);
+            } else if (outputValue.equalsIgnoreCase("BGN")) {
+                double result = money * poundRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("USD")) {
+                double result = (money * poundRate) / dollarRate;
+                System.out.println(round.format(result));
+            } else if (outputValue.equalsIgnoreCase("EUR")) {
+                double result = (money * poundRate) / euroRate;
+                System.out.println(round.format(result));
+            }
         }
     }
-}
 }
